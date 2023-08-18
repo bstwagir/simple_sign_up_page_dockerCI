199a63d65b7484c25b2c74a5d3f931dd3dbd67ae
bstwagir <bstwagir@mtu.edu>
2023-08-19 01:58:36 +0200

    feat: codebase and database of app

---
+++ UI/js/LogIn.js
@@ -0,0 +1,87 @@
+var requestOptions = {
+    method: 'GET',
+    redirect: 'follow'
+  };
+  
+  fetch("http://localhost:5000/server/users", requestOptions)
+  .then(response => response.text())
+  .then(result => {console.log(result), localStorage.setItem('signupData', result)})
+  .catch(error => console.log('error', error));
+  
+
+function Login() {
+    let email = document.getElementById('email').value, password = document.getElementById('password').value;
+    /**let formData = JSON.parse(localStorage.getItem('signupData')) || [];
+    let exist = formData.length && 
+    JSON.parse(localStorage.getItem('signupData')).some(data => data.email.toLowerCase() === email && data.password.toLowerCase() === password);
+    if(!exist){
+        alert("Incorrect login credentials");
+    }
+    else{*/
+        const myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+        myHeaders.append("Access-Control-Allow-Origin", "https://mybrandserver.onrender.com/")
+        const data = {
+            email: email,
+            password: password,
+        };
+    
+    
+        requestOptions = {
+            method: "POST",
+            headers: myHeaders,
+            body: JSON.stringify(data),
+            redirect: "follow",
+        };
+    
+        console.log(data, email.value);
+        fetch("http://localhost:5000/server/auth/login", requestOptions)
+            .then((response) => response.json())
+            .then((result) => { console.log(result), localStorage.setItem('current_user', JSON.stringify(result))})
+            .catch((error) => console.log("error", error));
+
+            let result;
+setTimeout(function () {
+  result = JSON.parse(localStorage.getItem('current_user'));
+  
+  if (result === 'Wrong credentials!') {
+    alert('Something went wrong, Please verify your credentials and try again');
+  } else if (result.others.isAdmin) {
+    location.href = "./Dashboard.html";
+  } else if (result) {
+    location.href = "./Dashboard.html";
+    document.querySelector(".button").innerHTML = result.others.name;
+  } else {
+    alert('Something went wrong, please try again');
+  }
+}, 2000);
+        
+    }
+        
+    
+
+/**
+function Login()
+{
+    var getEmail = JSON.parse(localStorage.getItem('userEmail'));
+    var getPass = JSON.parse(localStorage.getItem('userPass'));
+    
+    var enterEmail = document.getElementById('email').value;
+    var enterPass = document.getElementById('psw').value;
+    
+   
+
+    if( enterEmail==getEmail) {
+
+        if(enterPass==getPass){
+
+            alert('Log In Successfully')
+
+        }else(
+            alert('Wrong Password')
+        )
+    }else(
+        alert('Wrong Email')
+    )
+    
+}*/
