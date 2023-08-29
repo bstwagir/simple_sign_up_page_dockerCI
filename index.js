fed7520da508f93982853b5b044add31a0f468c2
bstwagir <bstwagir@mtu.edu>
2023-08-29 10:07:30 +0200

    feat: backup and restor

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ const options = {
 	definition: {
 		openapi: "3.0.0",
 		info: {
-			title: "My Brand API",
+			title: "Tekki-rw API",
 			version: "1.0.0",
 			description: "A simple Express API",
 		},
